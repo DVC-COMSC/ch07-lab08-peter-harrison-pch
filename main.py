189287b4c1076fb09727cb4c4581952cb451c27d
Peter Harrison <89763136+peter-harrison-pch@users.noreply.github.com>
2022-10-26 01:23:45 +0000

    insert with print of list after

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 # ******************************
 # Make your Code
 # ******************************
+ascending = input("numbers in ascending order: ").split()
+ascending = list(map(int, ascending))
+#ascending.sort() 
+
+single = int(input("single number: "))
+for i in range(len(ascending)):
+    if single < ascending[i]:
+        ascending.insert(i, single)
+        break
+
+print(ascending)
