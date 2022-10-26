6a73c694a8b531ac44b33aa6d0b251aa87d81db6
Peter Harrison <89763136+peter-harrison-pch@users.noreply.github.com>
2022-10-25 18:43:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,5 +9,8 @@ for i in range(len(ascending)):
     if single < ascending[i]:
         ascending.insert(i, single)
         break
+ 
+if single > ascending[-1]:
+    ascending.append(single)
 
 print(ascending)
